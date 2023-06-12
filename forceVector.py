1c01665be1fb03658a5c9683cfeb5253ce29a1a7
VolodymyrShabat <volodymyrshabat@gmail.com>
2023-06-13 01:44:17 +0300

    help

--- forceVector.py
+++ forceVector.py
@@ -21,13 +21,14 @@ def createDPSITE():
         DPSITE[i][1][7] = deltaPsi.deltaPsiTau68(nineNodes[i][0], nineNodes[i][1], 7)
     return DPSITE
 
-def createDPSIXYZ(DPSITE, AKT, NT, finiteElementIndex, side):
+def createDPSIXYZ(DPSITE, AKT, NT, npq, side):
     DPSIXYZ = np.zeros((9, 2, 3))
     for i in range(9):
-        for j in range(2):
-            for k in range(3):
-                for s in range(8):
-                    DPSIXYZ[i][j][k] += AKT[k][NT[helpers.sideToPoint[side][s]][finiteElementIndex]] * DPSITE[i][j][s]
+            for j in range(2):
+                for k in range(3):
+                    #change here for F
+                    for s in range(8):
+                        DPSIXYZ[i][j][k] += AKT[k][NT[helpers.sideToPoint[side][s]][npq]] * DPSITE[i][j][s]
     return DPSIXYZ
 
 def createFe1(DPSITE, AKT, NT, ZPi, permutationIndex):
@@ -35,11 +36,13 @@ def createFe1(DPSITE, AKT, NT, ZPi, permutationIndex):
     fe1 = [0] * 8
     DPSIXYZ = createDPSIXYZ(DPSITE, AKT, NT, ZPi[0], ZPi[1])
     for i in range(8):
-        for m in range(3):
-            for n in range(3):
-                pIndex = permutationIndex[(helpers.xg[m], helpers.xg[n])]
-                nul = DPSIXYZ[pIndex][0][1] * DPSIXYZ[pIndex][1][2] - DPSIXYZ[pIndex][0][2] * DPSIXYZ[pIndex][1][1]
-                fe1[i] += helpers.c[m] * helpers.c[n] * Pn * nul * psi.psii(helpers.xg[m], helpers.xg[n], i, i)
+        if i==0 or i==1 or i==4:
+            Pn = ZPi[2] if i == 4 else -ZPi[2]
+            for m in range(3):
+                for n in range(3):
+                    pIndex = permutationIndex[(helpers.xg[m], helpers.xg[n])]
+                    nul = DPSIXYZ[pIndex][0][1] * DPSIXYZ[pIndex][1][2] - DPSIXYZ[pIndex][0][2] * DPSIXYZ[pIndex][1][1]
+                    fe1[i] += helpers.c[m] * helpers.c[n] * Pn * nul * psi.psii(helpers.xg[m], helpers.xg[n], i, i)
     return fe1
 
 
@@ -48,10 +51,12 @@ def createFe2(DPSITE, AKT, NT, ZPi, permutationIndex):
     fe2 = [0] * 8
     DPSIXYZ = createDPSIXYZ(DPSITE, AKT, NT, ZPi[0], ZPi[1])
     for i in range(8):
-        for m in range(3):
-            for n in range(3):
-                pIndex = permutationIndex[(helpers.xg[m], helpers.xg[n])]
-                fe2[i] += helpers.c[m] * helpers.c[n] * Pn * (DPSIXYZ[pIndex][0][2] * DPSIXYZ[pIndex][1][0] - DPSIXYZ[pIndex][0][0] * DPSIXYZ[pIndex][1][2]) * psi.psii(helpers.xg[m], helpers.xg[n], i, i)
+        if i==0 or i==1 or i==4:
+            Pn = ZPi[2] if i == 4 else -ZPi[2]
+            for m in range(3):
+                for n in range(3):
+                    pIndex = permutationIndex[(helpers.xg[m], helpers.xg[n])]
+                    fe2[i] += helpers.c[m] * helpers.c[n] * Pn * (DPSIXYZ[pIndex][0][2] * DPSIXYZ[pIndex][1][0] - DPSIXYZ[pIndex][0][0] * DPSIXYZ[pIndex][1][2]) * psi.psii(helpers.xg[m], helpers.xg[n], i, i)
     return fe2
 
 
@@ -60,10 +65,12 @@ def createFe3(DPSITE, AKT, NT, ZPi, permutationIndex):
     fe3 = [0] * 8
     DPSIXYZ = createDPSIXYZ(DPSITE, AKT, NT, ZPi[0], ZPi[1])
     for i in range(8):
-        for m in range(3):
-            for n in range(3):
-                pIndex = permutationIndex[(helpers.xg[m], helpers.xg[n])]
-                fe3[i] += helpers.c[m] * helpers.c[n] * Pn * (DPSIXYZ[pIndex][0][0] * DPSIXYZ[pIndex][1][1] - DPSIXYZ[pIndex][0][1] * DPSIXYZ[pIndex][1][0]) * psi.psii(helpers.xg[m], helpers.xg[n], i, i)
+            if i==0 or i==1 or i==4:
+                Pn = ZPi[2] if i == 4 else -ZPi[2]
+                for m in range(3):
+                    for n in range(3):
+                        pIndex = permutationIndex[(helpers.xg[m], helpers.xg[n])]
+                        fe3[i] += helpers.c[m] * helpers.c[n] * Pn * (DPSIXYZ[pIndex][0][0] * DPSIXYZ[pIndex][1][1] - DPSIXYZ[pIndex][0][1] * DPSIXYZ[pIndex][1][0]) * psi.psii(helpers.xg[m], helpers.xg[n], i, i)
     return fe3
 
 
