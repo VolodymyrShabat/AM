47267c09278b9c17132802289f0dcc34e14e805a
VolodymyrShabat <volodymyrshabat@gmail.com>
2023-06-13 01:30:18 +0300

    added text

--- globalMatrix.py
+++ globalMatrix.py
@@ -1,20 +1,31 @@
 import helpers
 
+#F
+#x1 y1 z1 x2 y2 z2 ...
+
+#FE
+#x1 x2 x3 x4 ... y1 y2 y3 y4 ... z1 z2 z3 z4
 def initGlobals(MG, vecF, allMGE, allFE, ZP, NT):
     for q in range(len(allMGE)):
         for i in range(len(allMGE[0])):
             for j in range(len(allMGE[0])):
-                i1 = NT[i % 20][q] * 3 + i // 20
-                i2 = NT[j % 20][q] * 3 + j // 20
+                i1 = NT[i % 20][q] * 3 + i // 20 #[i % 20] - move xn,yn,zn  i // 20 - move between x,y,z
+                i2 = NT[j % 20][q] * 3 + j // 20 # same but for column
                 MG[i1][i2] += allMGE[q][i][j]
 
     for q in range(len(ZP)):
+        print(len(allFE[0]))
         for i in range(len(allFE[0])):
-            comp = i // 20
-            i1 = NT[i % 20][ZP[q][0]] * 3 + comp
+            i1 = NT[i % 20][ZP[q][0]] * 3 +  i // 20
             vecF[i1] += allFE[q][i]
 
 
+#MG
+#x1 x2 x3
+#x2
+#x3
+
+#MG
 #x1 y1 z1
 #y1
 #z1
